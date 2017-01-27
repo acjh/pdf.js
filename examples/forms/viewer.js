67c82c299e1246ffd051abeb24d610c26d4445a9
Aaron Chong <aaronjhchong@hotmail.com>
2017-01-27 10:34:14 +0800

    Show data in real-time

---
+++ examples/forms/viewer.js
@@ -0,0 +1,38 @@
+/*
+ * Loads jQuery
+ */
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+/*
+ * Trims field names to actual names on AcroForm
+ */
+function trimFieldNames(data) {
+    dict = {};
+    for (var id in data) {
+      keys = id.split('.');
+      key = keys[keys.length - 1];
+      dict[key] = data[id];
+    }
+    return dict
+}
+
+/*
+ * Removes link for "Show me all the data!"
+ * Show updated data in 'div#info' on input
+ */
+function showDataInRealTime() {
+    $(':not(#pager) > a').remove();
+
+    $('input').on('input', function() {
+        var data = FormsDemo.getPdfData();
+        data = trimFieldNames(data);
+        FormsDemo.dumpDataToDiv('info',data);
+    });
+}
+
+$(document).ready(function() {
+    showDataInRealTime();
+});
