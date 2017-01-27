3b24fd497f0e0b503f0176a334c801001a51e70d
Aaron Chong <aaronjhchong@hotmail.com>
2017-01-27 17:50:55 +0800

    viewer.js: Show id and value only for updated field

--- examples/forms/viewer.js
+++ examples/forms/viewer.js
@@ -1,23 +1,10 @@
 /*
- * Trims field names to actual names on AcroForm
- */
-function trimFieldNames(data) {
-    dict = {};
-    for (var id in data) {
-      keys = id.split('.');
-      key = keys[keys.length - 1];
-      dict[key] = data[id];
-    }
-    return dict
-}
-
-/*
  * Show updated data in 'div#info' on input
  */
 function showDataInRealTime() {
     $('input').on('input', function() {
-        var data = FormsDemo.getPdfData();
-        data = trimFieldNames(data);
+        var data = {};
+		data[this.id] = this.value;
         FormsDemo.dumpDataToDiv('info',data);
     });
 }
