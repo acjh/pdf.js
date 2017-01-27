a866c00ff64fbd6c1349d70eea4abe6c79b4a6c8
Aaron Chong <aaronjhchong@hotmail.com>
2017-01-27 10:54:42 +0800

    index.html: Load jQuery in script tag

--- examples/forms/viewer.js
+++ examples/forms/viewer.js
@@ -1,12 +1,4 @@
 /*
- * Loads jQuery
- */
-var script = document.createElement('script');
-script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
-/*
  * Trims field names to actual names on AcroForm
  */
 function trimFieldNames(data) {
